911d61f3c15544b1a578b0b0782275f68bbaf29b
pedroispc <ispcpedroghirardi@gmail.com>
2022-06-07 11:55:58 -0300

    holis

---
+++ Ejercicio 1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hola juli")
+#GUIA DE EJERCICIOS: CLASE 1
+#1
+valor1 = int(input("ingrese el primer valor: "))
+valor2 = int(input("ingtrese el segundo valor: "))
+valor3 = int(input("ingrese el tercer valor: "))
+#CALCULOS
+calculo1 = valor1 + valor2
+calculo2 = valor1 + valor3
+
+
+print("el calculo 1: ", calculo1)
+print("el calculo 2: ", calculo2)
+
+calculo3 = valor1 + valor2 / valor3
+
+calculo4 = calculo3 + 10
+print("El valor del calculo 4: ", calculo4)
+
+tipoDato = type(calculo4)
+
+print(tipoDato)
